45dad210f474acab7a14498650353a701dc2c431
jeanport <pierreanatc522@gmail.com>
2025-04-17 15:04:17 -0300

    Criando classe de ErrorResponse

---
+++ src/main/java/com/ppsolution/ecovendas/dto/response/ErrorResponse.java
@@ -0,0 +1,6 @@
+package com.ppsolution.ecovendas.dto.response;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(String error, String message, Integer status, String cause, LocalDateTime timesTamp) {
+}
