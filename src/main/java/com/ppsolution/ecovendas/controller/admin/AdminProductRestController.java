1c2196cb734c2545d32dbceb1640de4aa0cab741
jeanport <pierreanatc522@gmail.com>
2025-04-24 10:07:58 -0300

    Criando Product Controller

---
+++ src/main/java/com/ppsolution/ecovendas/controller/admin/AdminProductRestController.java
@@ -0,0 +1,11 @@
+package com.ppsolution.ecovendas.controller.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin/products")
+public class AdminProductRestController {
+
+
+}
