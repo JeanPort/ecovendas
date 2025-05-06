7f044a6a44e79d2078f7a07e4c7ac67181547425
jeanport <pierreanatc522@gmail.com>
2025-05-06 09:23:39 -0300

    Criando TokenServiceException

---
+++ src/main/java/com/ppsolution/ecovendas/service/token/TokenServiceException.java
@@ -0,0 +1,7 @@
+package com.ppsolution.ecovendas.service.token;
+
+public class TokenServiceException extends RuntimeException{
+    public TokenServiceException(String message) {
+        super(message);
+    }
+}
